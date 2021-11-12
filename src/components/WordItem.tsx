5286b828bd6543169dbe62440528476c804dd1ca
Oleg Kan <kan.fcsd@gmail.com>
2021-11-12 18:35:29 +0200

    Added the word list display

---
+++ src/components/WordItem.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Word } from "../types/types";
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+
+type WordItemProps = {
+  word: Word;
+  index: number;
+};
+
+export const WordItem = ({ word, index }: WordItemProps) => {
+  return (
+    <TableRow key={word.key}>
+      <TableCell component="th" scope="row">
+        {index}
+      </TableCell>
+      <TableCell>{word.eng}</TableCell>
+      <TableCell>{word.rus}</TableCell>
+    </TableRow>
+  );
+};
